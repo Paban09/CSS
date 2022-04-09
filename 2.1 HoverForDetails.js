01576079d42ff4af5f492e158de337a7bd06fca3
Paban09 <masudranapaban22@gmail.com>
2022-04-09 21:44:02 +0600

    css

---
+++ 2.1 HoverForDetails.js
@@ -0,0 +1,11 @@
+
+    var gForm= false;
+    function get(id){
+        return document.getElementById(id);
+    }
+    function showDetails(){
+        get("info").style.display="block";
+    }
+    function hideDetails(){
+        get("info").style.display="none";
+    }
